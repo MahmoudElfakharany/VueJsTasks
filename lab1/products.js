92f792160b0b6f5d6977e8b971186d9224713788
MahmoudElfakharany <m.a.el-fakharany@hotmail.com>
2023-05-06 22:46:35 +0300

    first commit

---
+++ lab1/products.js
@@ -0,0 +1,88 @@
+Products = [
+  {
+    id: 1,
+    productName: "Mawada",
+    category: "Life Style",
+    numberOfPages: 100,
+    author: "Ali",
+    price: 300,
+
+    imgUrl:
+      "https://thebookcoverdesigner.com/wp-content/uploads/2022/05/book-cover-171.jpg",
+  },
+  {
+    id: 6,
+    productName: "Rahma",
+    category: "Life Style",
+    numberOfPages: 150,
+    author: "Kareem",
+    price: 100,
+
+    imgUrl:
+      "https://thebookcoverdesigner.com/wp-content/uploads/2022/08/book-cover-143.jpg",
+  },
+  {
+    id: 7,
+    productName: "Islam",
+    category: "Religion",
+    numberOfPages: 200,
+    author: "Hassan",
+    price: 300,
+
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTggYKHTEItw2i9lxeVdx9lehCceTRvYnuNLmWPbUf-N20CRnPVhVkPz_g224AkzLSYwog&usqp=CAU",
+  },
+  {
+    id: 2,
+    productName: "Rage",
+    category: "psychology",
+    numberOfPages: 30,
+    author: "Ali",
+    price: 300,
+
+    imgUrl:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTR1arxzYHfJwhR3-62yGA73pBkgjE7i2jjQ_C-9Ah7i16FDIURFcs9exwse26N3KMsI1w&usqp=CAU",
+  },
+  {
+    id: 3,
+    productName: "Men & Women",
+    category: "Life Style",
+    numberOfPages: 120,
+    author: "Ahmed",
+    price: 300,
+
+    imgUrl:
+      "https://thebookcoverdesigner.com/wp-content/uploads/2022/09/book-cover-136.jpg",
+  },
+  {
+    id: 4,
+    productName: "Secrets",
+    category: "Mystery",
+    numberOfPages: 20,
+    author: "Fathy",
+    price: 300,
+
+    imgUrl:
+      "https://thebookcoverdesigner.com/wp-content/uploads/2022/09/book-cover-124.jpg",
+  },
+  {
+    id: 5,
+    productName: "Politics Laws",
+    category: "Politics",
+    numberOfPages: 180,
+    author: "Mostafa",
+    price: 300,
+    imgUrl:
+      "https://thebookcoverdesigner.com/wp-content/uploads/2022/01/book-cover-209-324x519.jpg",
+  },
+  {
+    id: 17,
+    productName: "Stars",
+    category: "Astrology",
+    numberOfPages: 170,
+    author: "Sara",
+    price: 100,
+    imgUrl:
+      "https://thebookcoverdesigner.com/wp-content/uploads/2022/02/book-cover-182.jpg",
+  },
+];
